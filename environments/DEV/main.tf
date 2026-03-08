b213f84c3d5bcf40b5b4480936975563816dab2a
Maxime <m.djouchy@gmail.com>
2026-03-08 13:17:49 -0400

    fixed the code using modules

--- environments/DEV/main.tf
+++ environments/DEV/main.tf
@@ -1,10 +1,46 @@
+terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-mgmt"
+    storage_account_name = "tfstatemaxdjou6428"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.116, < 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "my_devops_rg" {
+  name     = "rg-dev"
+  location = "East US"
+}
+
 module "network" {
-  source = "../../modules/network"
+  source  = "../../modules/network"
+  rg_name = azurerm_resource_group.my_devops_rg.name
 }
 
 module "vm_dev" {
-  source  = "../../modules/compute"
-  vm_name = "vm-dev-01"
-  rg_name = "rg-dev"
-  # ...
+  source         = "../../modules/compute"
+  vm_name        = "vm-dev-01"
+  vm_size        = "Standard_B1s"
+  rg_name        = azurerm_resource_group.my_devops_rg.name
+  location       = azurerm_resource_group.my_devops_rg.location
+  subnet_id      = module.network.subnet_id
+  ssh_public_key = var.ssh_public_key
 }
